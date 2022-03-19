a7e9a0359df72b7fa96e195fa7a06ac943b67613
ALVARO NEYRA <N00167675@UPN.PE>
2022-03-18 19:00:51 -0500

    insertar imagen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,33 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
-function Footer() {
+
+ function Footer() {
     return (
-        <div>
-            <h1>My First React App</h1>
-        </div>
-    );
-}
+    <Box
+        px={{ xs: 3, sm: 10 }}
+        py={{ xs: 5, sm: 10 }}
+        bgcolor="text.secondary"
+        color="white"
+    >
+  <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              <Box>2022</Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box>Desarrollado con ❤ por: Nadia, Alvaro, Jian y Jairo </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box><img src={github}/></Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    
+);
 
+}
 export default Footer;
