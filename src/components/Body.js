054b720f04acaf8193110c11652dd585a35d1b10
Nadia Mendoza <nadia_mendoza_67@hotmail.com>
2022-03-25 15:13:35 -0500

    cambio modal

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,78 +6,7 @@ import IconButton from "@mui/material/IconButton";
 import { Tooltip } from "@mui/material";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import Modal from "@mui/material/Modal";
-import Link from '@mui/material/Link';
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import conejo from "./images/conejo.png";
-import elefante from "./images/elefante.png";
-import gato from "./images/gato.png";
-import leon from "./images/leon.png";
-import oso from "./images/oso.png";
-import patos from "./images/patos.png";
-import perro from "./images/perro.png";
-import pollo from "./images/pollo.png";
-import tigre from "./images/tigre.png";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "auto",
-  bgcolor: "background.paper",
-  border: "1px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
-const itemData = [
-  {
-    img: conejo,
-    title: "Conejo",
-    desc: "Conejo",
-  },
-  {
-    img: elefante,
-    title: "Elefante",
-    desc: "Elefante",
-  },
-  {
-    img: gato,
-    title: "Gato",
-    desc: "Gato",
-  },
-  {
-    img: leon,
-    title: "Leon",
-    desc: "Leon",
-  },
-  {
-    img: oso,
-    title: "Oso",
-    desc: "Oso",
-  },
-  {
-    img: patos,
-    title: "Patos",
-    desc: "Patos",
-  },
-  {
-    img: perro,
-    title: "Perro",
-    desc: "Perro",
-  },
-  {
-    img: pollo,
-    title: "Pollo",
-    desc: "Pollo",
-  },
-  {
-    img: tigre,
-    title: "Tigre",
-    desc: "Tigre",
-  },
-];
+import ModalPatas  from "./ModalPatas";
 
 function Body() {
   const [open, setOpen] = React.useState(false);
@@ -128,21 +57,7 @@ function Body() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-            {itemData.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          <Link href="https://www.flaticon.es/packs/footprints-2" title="iconos" color="inherit">Iconos creados por surang - Flaticon</Link>
-        </Box>
+        <ModalPatas />
       </Modal>
     </div>
   );
---
+++ src/components/ModalPatas.js
@@ -0,0 +1,134 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
+import conejo from "./images/conejo.png";
+import elefante from "./images/elefante.png";
+import gato from "./images/gato.png";
+import leon from "./images/leon.png";
+import oso from "./images/oso.png";
+import patos from "./images/patos.png";
+import perro from "./images/perro.png";
+import tigre from "./images/tigre.png";
+import mono from "./images/mono.png";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      main: grey[200],
+    },
+    text: {
+        primary: grey[900],
+      },
+  },
+});
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "background.paper",
+  border: "1px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const itemData = [
+  {
+    img: conejo,
+    title: "Conejo",
+    desc: "Conejo",
+  },
+  {
+    img: elefante,
+    title: "Elefante",
+    desc: "Elefante",
+  },
+  {
+    img: gato,
+    title: "Gato",
+    desc: "Gato",
+  },
+  {
+    img: leon,
+    title: "Leon",
+    desc: "Leon",
+  },
+  {
+    img: oso,
+    title: "Oso",
+    desc: "Oso",
+  },
+  {
+    img: patos,
+    title: "Pato",
+    desc: "Pato",
+  },
+  {
+    img: perro,
+    title: "Perro",
+    desc: "Perro",
+  },
+  {
+    img: tigre,
+    title: "Tigre",
+    desc: "Tigre",
+  },
+  {
+    img: mono,
+    title: "Mono",
+    desc: "Mono",
+  },
+];
+
+function ModalPatas() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={style}>
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={120}>
+          {itemData.map((item) => (
+            <Button sx={{ p: 0 }}>
+              <ImageListItem
+                key={item.img}
+                sx={{
+                  p: 2,
+                  mb: 3,
+                  bgcolor: "background.main",
+                  borderRadius: "16px",
+                }}
+              >
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <Typography
+                  sx={{ display: "flex", justifyContent: "center", mt: 1, color: "text.primary" }}
+                >
+                  {item.title}
+                </Typography>
+              </ImageListItem>
+            </Button>
+          ))}
+        </ImageList>
+        <Link
+          href="https://www.flaticon.es/packs/footprints-2"
+          title="iconos"
+          color="inherit"
+        >
+          Iconos creados por surang - Flaticon
+        </Link>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default ModalPatas;
