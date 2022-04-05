3f85aa68b7ea4f9ddf8790cf768e73c8cf744db5
jiancarlos <jiancarlosmarcosp@gmail.com>
2022-04-05 18:00:47 -0500

    .

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,6 @@ import React, { useContext, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import FirmaPatas from "./FirmaPatas";
-// import { CKEditor } from "@ckeditor/ckeditor5-react";
-// import { ClassicEditor } from "@ckeditor/ckeditor5-build-classic";
-// Require Editor JS files.
 import "froala-editor/js/froala_editor.pkgd.min.js";
 
 // Require Editor CSS files.
@@ -14,13 +11,6 @@ import "froala-editor/css/froala_editor.pkgd.min.css";
 import FroalaEditor from "react-froala-wysiwyg";
 import { DataContext } from "../context/DataContext";
 import AgregarFirmaPata from "./scripts/AgregarFirmaPata";
-
-// Include special components if required.
-// import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
-// import FroalaEditorA from 'react-froala-wysiwyg/FroalaEditorA';
-// import FroalaEditorButton from 'react-froala-wysiwyg/FroalaEditorButton';
-// import FroalaEditorImg from 'react-froala-wysiwyg/FroalaEditorImg';
-// import FroalaEditorInput from 'react-froala-wysiwyg/FroalaEditorInput';
 
 const Body = () => {
   const { setContenido } = useContext(DataContext);
